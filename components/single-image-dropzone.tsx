e073d61a077c527b8b4122e1ece4a387ea1281bb
Urvashi <105716047+urvashi-a20@users.noreply.github.com>
2024-12-24 21:56:24 +0530

    Add files via upload

---
+++ components/single-image-dropzone.tsx
@@ -0,0 +1,206 @@
+'use client';
+
+import { UploadCloudIcon, X } from 'lucide-react';
+import * as React from 'react';
+import { useDropzone, DropzoneOptions } from 'react-dropzone';
+import { twMerge } from 'tailwind-merge';
+
+import { Spinner } from './spinner';
+
+const variants = {
+  base: 'relative rounded-md flex justify-center items-center flex-col cursor-pointer min-h-[150px] min-w-[200px] border border-dashed border-gray-400 dark:border-gray-300 transition-colors duration-200 ease-in-out',
+  image: 'border-0 p-0 min-h-0 min-w-0 relative shadow-md bg-slate-200 dark:bg-slate-900 rounded-md',
+  active: 'border-2',
+  disabled: 'bg-gray-200 border-gray-300 cursor-default pointer-events-none bg-opacity-30 dark:bg-gray-700',
+  accept: 'border border-blue-500 bg-blue-500 bg-opacity-10',
+  reject: 'border border-red-700 bg-red-700 bg-opacity-10',
+};
+
+type InputProps = {
+  width?: number;
+  height?: number;
+  className?: string;
+  value?: File | string;
+  onChange?: (file?: File) => void | Promise<void>;
+  disabled?: boolean;
+  dropzoneOptions?: Omit<DropzoneOptions, 'disabled'>;
+};
+
+const ERROR_MESSAGES = {
+  fileTooLarge(maxSize: number) {
+    return `The file is too large. Max size is ${formatFileSize(maxSize)}.`;
+  },
+  fileInvalidType() {
+    return 'Invalid file type.';
+  },
+  tooManyFiles(maxFiles: number) {
+    return `You can only add ${maxFiles} file(s).`;
+  },
+  fileNotSupported() {
+    return 'The file is not supported.';
+  },
+};
+
+const SingleImageDropzone = React.forwardRef<HTMLInputElement, InputProps>(
+  (
+    { dropzoneOptions, width, height, value, className, disabled, onChange },
+    ref,
+  ) => {
+    const isBrowser = typeof window !== 'undefined';
+
+    const imageUrl = React.useMemo(() => {
+      if (!isBrowser) return null; // Ensure SSR safety
+      if (typeof value === 'string') {
+        return value; // Use external URL if provided
+      }
+      if (value instanceof File) {
+        return URL.createObjectURL(value); // Generate a URL for the file
+      }
+      return null;
+    }, [value, isBrowser]);
+
+    const {
+      getRootProps,
+      getInputProps,
+      acceptedFiles,
+      fileRejections,
+      isFocused,
+      isDragAccept,
+      isDragReject,
+    } = useDropzone({
+      accept: { 'image/*': [] },
+      multiple: false,
+      disabled,
+      onDrop: (acceptedFiles) => {
+        const file = acceptedFiles[0];
+        if (file) {
+          void onChange?.(file);
+        }
+      },
+      ...dropzoneOptions,
+    });
+
+    const dropZoneClassName = React.useMemo(
+      () =>
+        twMerge(
+          variants.base,
+          isFocused && variants.active,
+          disabled && variants.disabled,
+          imageUrl && variants.image,
+          (isDragReject ?? fileRejections[0]) && variants.reject,
+          isDragAccept && variants.accept,
+          className,
+        ).trim(),
+      [
+        isFocused,
+        imageUrl,
+        fileRejections,
+        isDragAccept,
+        isDragReject,
+        disabled,
+        className,
+      ],
+    );
+
+    const errorMessage = React.useMemo(() => {
+      if (fileRejections[0]) {
+        const { errors } = fileRejections[0];
+        if (errors[0]?.code === 'file-too-large') {
+          return ERROR_MESSAGES.fileTooLarge(dropzoneOptions?.maxSize ?? 0);
+        }
+        if (errors[0]?.code === 'file-invalid-type') {
+          return ERROR_MESSAGES.fileInvalidType();
+        }
+        if (errors[0]?.code === 'too-many-files') {
+          return ERROR_MESSAGES.tooManyFiles(dropzoneOptions?.maxFiles ?? 0);
+        }
+        return ERROR_MESSAGES.fileNotSupported();
+      }
+      return undefined;
+    }, [fileRejections, dropzoneOptions]);
+
+    React.useEffect(() => {
+      return () => {
+        if (imageUrl && typeof value !== 'string') {
+          URL.revokeObjectURL(imageUrl); // Clean up object URL to prevent memory leaks
+        }
+      };
+    }, [imageUrl, value]);
+
+    return (
+      <div className="relative">
+        {disabled && (
+          <div className="flex items-center justify-center absolute inset-y-0 h-full w-full bg-background/80 z-50">
+            <Spinner size="lg" />
+          </div>
+        )}
+        <div
+          {...getRootProps({
+            className: dropZoneClassName,
+            style: {
+              width,
+              height,
+            },
+          })}
+        >
+          <input ref={ref} {...getInputProps()} />
+
+          {imageUrl ? (
+            <img
+              className="h-full w-full rounded-md object-cover"
+              src={imageUrl}
+              alt={acceptedFiles[0]?.name || 'Uploaded image'}
+            />
+          ) : (
+            <div className="flex flex-col items-center justify-center text-xs text-gray-400">
+              <UploadCloudIcon className="mb-2 h-7 w-7" />
+              <div className="text-gray-400">
+                Click or drag file to this area to upload
+              </div>
+            </div>
+          )}
+
+          {imageUrl && !disabled && (
+            <button
+              type="button"
+              className="group absolute right-0 top-0 -translate-y-1/4 translate-x-1/4 transform"
+              onClick={(e) => {
+                e.stopPropagation();
+                void onChange?.(undefined);
+              }}
+            >
+              <div className="flex h-5 w-5 items-center justify-center rounded-md border border-solid border-gray-500 bg-white transition-all duration-300 hover:h-6 hover:w-6 dark:border-gray-400 dark:bg-black">
+                <X
+                  className="text-gray-500 dark:text-gray-400"
+                  width={16}
+                  height={16}
+                />
+              </div>
+            </button>
+          )}
+        </div>
+
+        <div className="mt-1 text-xs text-red-500">{errorMessage}</div>
+      </div>
+    );
+  },
+);
+SingleImageDropzone.displayName = 'SingleImageDropzone';
+
+function formatFileSize(bytes?: number) {
+  if (!bytes) {
+    return '0 Bytes';
+  }
+  const normalizedBytes = Number(bytes);
+  if (normalizedBytes === 0) {
+    return '0 Bytes';
+  }
+  const k = 1024;
+  const dm = 2;
+  const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+  const i = Math.floor(Math.log(normalizedBytes) / Math.log(k));
+
+  return `${Number.parseFloat((normalizedBytes / k ** i).toFixed(dm))} ${sizes[i]}`;
+}
+
+export { SingleImageDropzone };
